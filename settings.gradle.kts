bd1d70287d0f46443886210b621ccac310e65bef
rajeevranganathan <rajeevkr878@gmail.com>
2025-09-18 00:52:55 +0530

    feat: Add core and feature modules
    - Add core module with basic setup and test files
    - Add feature module with auth submodule
    - Configure build files and dependencies for new modules
    - Add Android library plugin to project build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    includeBuild("build-logic")
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
